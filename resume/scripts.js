7c2a4222d0b1a91f7d9f44020ee7b0442cec54ef
Sam Dyer <samueljdyer@gmail.com>
2019-07-24 21:14:47 -0700

    missing object movement

---
+++ resume/scripts.js
@@ -0,0 +1,21 @@
+﻿
+function msgClear() {
+    document.getElementById("messageTextArea").value = "";
+    document.getElementById("nameMessage").value = "";
+    document.getElementById("emailMessage").value = "";
+}
+
+function msgSubmit() {
+    msgClear();
+    document.getElementById("messageSubmit").style.display = "none";
+    document.getElementById("messageClear").style.display = "none";
+    document.getElementById("hiddenDiv").style.display = "block";
+}
+
+function changeColor(elem) {
+    document.getElementById(elem).style.color = "red";
+}
+
+function changeColorBack(elem) {
+    document.getElementById(elem).style.color = "black";
+}
