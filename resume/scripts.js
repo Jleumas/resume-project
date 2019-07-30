a3c7e38556bfe9493782e42180abbccd32904a7f
Samuel Dyer <samueljdyer@LIVE.COM>
2019-07-30 15:23:47 -0700

    dynamic header working

--- resume/scripts.js
+++ resume/scripts.js
@@ -1,4 +1,45 @@
-﻿
+﻿var x = 0;
+var y = 0;
+var radius = 100;
+var angleDeg = 0;
+var originx = 500;
+var originy = 500;
+var delay = 20;
+
+function degreesToRad(degrees) {
+    return (degrees * Math.PI / 180);
+}
+
+function circle(angle) {
+    var newAngle = angle;
+    newAngle = degreesToRad(newAngle);
+    x = originx + radius * Math.cos(newAngle);
+    y = originy + radius * Math.sin(newAngle);
+}
+
+async function rotate(elem) {
+    var dom = document.getElementById(elem);
+    circle(angleDeg);
+    dom.style.left = x + 'px';
+    dom.style.top = y + 'px';
+
+    if (angleDeg < 360) {
+        await sleep(delay);
+        angleDeg += 1;
+        return rotate(elem);
+    }
+    else {
+        await sleep(delay);
+        angleDeg = 0;
+        return rotate(elem);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 function msgClear() {
     document.getElementById("messageTextArea").value = "";
     document.getElementById("nameMessage").value = "";
@@ -18,4 +59,14 @@ function changeColor(elem) {
 
 function changeColorBack(elem) {
     document.getElementById(elem).style.color = "black";
+}
+
+function loadNavBar(){
+    $("#navHook")  
+        .append("<li><a class=\"nav-link active\" data-toggle=\"tab\" href=\"#home\">Home</a></li>")
+        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#experience\">Experience</a></li>")
+        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#skills\">Skills</a></li>")
+        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#education\">Education</a></li>") 
+        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#personal\">Personal</a></li>")
+        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#contact\">Contact</a></li>");
 }
