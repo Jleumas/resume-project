2e79fd6a933228206aa062538699f0e2e62d9355
Sam Dyer <samueljdyer@gmail.com>
2019-08-01 22:12:04 -0700

    version 0.1

--- resume/scripts.js
+++ resume/scripts.js
@@ -62,11 +62,40 @@ function changeColorBack(elem) {
 }
 
 function loadNavBar(){
-    $("#navHook")  
-        .append("<li><a class=\"nav-link active\" data-toggle=\"tab\" href=\"#home\">Home</a></li>")
-        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#experience\">Experience</a></li>")
-        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#skills\">Skills</a></li>")
-        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#education\">Education</a></li>") 
-        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#personal\">Personal</a></li>")
-        .append("<li><a class=\"nav-link\" data-toggle=\"tab\" href=\"#contact\">Contact</a></li>");
+    //build top level nav element
+    $("#navBarHook")
+        .append("<a class=\"navbar-brand\" id=\"navBarBrand\" href=\"index.html\">Sam Dyer</a>");
+
+    //add hamburger button
+    $("#navBarHook")
+        .append("<button id=\"hamburgerButton\" class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\"></button>");
+
+    $("#hamburgerButton")
+        .append("<span class=\"navbar-toggler-icon\"></span>");
+    
+    //create container for list
+    $("#navBarHook")
+        .append("<div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\"></div>");
+
+    //create list to insert links
+    $("#navbarSupportedContent")  
+        .append("<ul class=\"navbar-nav\" id=\"navbarList\"></ul>");
+
+    //add links
+    $("#navbarList")
+        .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"\experience.html\">Experience</a></li>")
+        .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"\skills.html\">Skills</a></li>")
+        .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"\education.html\">Education</a></li>") 
+        .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"\portfolio.html\">Portfolio</a></li>")
+        .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"\personal.html\">Personal</a></li>")
+        .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"\contact.html\">Contact</a></li>");
+    
+    //add spacing to end of document.
+    //replace with dynamically generated footer in the future.
+    $("body").append("<br/>");
+}
+
+
+function loadNavBarList(){
+    
 }
